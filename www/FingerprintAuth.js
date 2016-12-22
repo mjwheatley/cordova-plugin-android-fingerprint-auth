6a5c55347420acb126713860ded206ead5ae602b
simone-compagnone <simone-compagnone@users.noreply.github.com>
2016-12-22 11:42:11 +0100

    Adding `init` & `delete` functions

--- www/FingerprintAuth.js
+++ www/FingerprintAuth.js
@@ -1,27 +1,53 @@
-function FingerprintAuth() {
-}
+cordova.define("cordova-plugin-android-fingerprint-auth.FingerprintAuth", function(require, exports, module) {
+  function FingerprintAuth() {
+  }
 
-FingerprintAuth.prototype.show = function (params, successCallback, errorCallback) {
-    cordova.exec(
-        successCallback,
-        errorCallback,
-        "FingerprintAuth",  // Java Class
-        "authenticate", // action
-        [ // Array of arguments to pass to the Java class
-            params
-        ]
-    );
-};
+  FingerprintAuth.prototype.show = function (params, successCallback, errorCallback) {
+      cordova.exec(
+          successCallback,
+          errorCallback,
+          "FingerprintAuth",  // Java Class
+          "authenticate", // action
+          [ // Array of arguments to pass to the Java class
+              params
+          ]
+      );
+  };
 
-FingerprintAuth.prototype.isAvailable = function (successCallback, errorCallback) {
-    cordova.exec(
-        successCallback,
-        errorCallback,
-        "FingerprintAuth",  // Java Class
-        "availability", // action
-        [{}]
-    );
-};
+  FingerprintAuth.prototype.init = function (params, successCallback, errorCallback) {
+      cordova.exec(
+          successCallback,
+          errorCallback,
+          "FingerprintAuth",  // Java Class
+          "init", // action
+          [ // Array of arguments to pass to the Java class
+              params
+          ]
+      );
+  };
 
-FingerprintAuth = new FingerprintAuth();
-module.exports = FingerprintAuth;
+  FingerprintAuth.prototype.delete = function (params, successCallback, errorCallback) {
+      cordova.exec(
+          successCallback,
+          errorCallback,
+          "FingerprintAuth",  // Java Class
+          "delete", // action
+          [ // Array of arguments to pass to the Java class
+              params
+          ]
+      );
+  };
+
+  FingerprintAuth.prototype.isAvailable = function (successCallback, errorCallback) {
+      cordova.exec(
+          successCallback,
+          errorCallback,
+          "FingerprintAuth",  // Java Class
+          "availability", // action
+          [{}]
+      );
+  };
+
+  FingerprintAuth = new FingerprintAuth();
+  module.exports = FingerprintAuth;
+});
